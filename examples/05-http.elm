62ec8bb181f7a07b103b67213ea8602d3290b31c
Yaroslav Sapozhnyk <yaroslav.sapozhnik@gmail.com>
2017-03-27 22:58:58 -0400

    Implement exercises (2 of 3)
    Implement 'Show a message explaining why the image didn't change when you get an Http.Error.'
    Implement 'Allow the user to modify the topic with a text field.'

--- examples/05-http.elm
+++ examples/05-http.elm
@@ -22,12 +22,13 @@ main =
 type alias Model =
   { topic : String
   , gifUrl : String
+  , error : String
   }
 
 
 init : String -> (Model, Cmd Msg)
 init topic =
-  ( Model topic "waiting.gif"
+  ( Model topic "waiting.gif" ""
   , getRandomGif topic
   )
 
@@ -38,6 +39,7 @@ init topic =
 
 type Msg
   = MorePlease
+  | Topic String
   | NewGif (Result Http.Error String)
 
 
@@ -47,11 +49,14 @@ update msg model =
     MorePlease ->
       (model, getRandomGif model.topic)
 
-    NewGif (Ok newUrl) ->
-      (Model model.topic newUrl, Cmd.none)
+    Topic topic ->
+      ({ model | topic = topic }, Cmd.none)
 
-    NewGif (Err _) ->
-      (model, Cmd.none)
+    NewGif (Ok newUrl) ->
+      (Model model.topic newUrl "", Cmd.none)
+
+    NewGif (Err error) ->
+      ({ model | error = decodeError error }, Cmd.none)
 
 
 
@@ -62,9 +67,12 @@ view : Model -> Html Msg
 view model =
   div []
     [ h2 [] [text model.topic]
+    , input [ type_ "text" , onInput Topic, value model.topic][]
+    , br [] []
     , button [ onClick MorePlease ] [ text "More Please!" ]
     , br [] []
     , img [src model.gifUrl] []
+    , div [] [ text model.error ]
     ]
 
 
@@ -93,3 +101,18 @@ getRandomGif topic =
 decodeGifUrl : Decode.Decoder String
 decodeGifUrl =
   Decode.at ["data", "image_url"] Decode.string
+
+
+decodeError : Http.Error -> String
+decodeError error =
+  case error of
+    Http.BadUrl string ->
+      "Bad URL"
+    Http.Timeout ->
+      "Timeout occured"
+    Http.NetworkError ->
+      "NetworkError"
+    Http.BadStatus response ->
+      response.status.message
+    Http.BadPayload data response ->
+      response.status.message
