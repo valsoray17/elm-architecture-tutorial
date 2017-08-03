616700dba2a413a301433ae49668722393f4e5e2
Yaroslav Sapozhnyk <yaroslav.sapozhnik@gmail.com>
2017-08-03 10:24:13 -0400

    Add time to html

--- examples/06-clock.elm
+++ examples/06-clock.elm
@@ -1,4 +1,4 @@
-import Html exposing (Html)
+import Html exposing (..)
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 import Time exposing (Time, second)
@@ -66,7 +66,9 @@ view model =
     handY =
       toString (50 + 40 * sin angle)
   in
-    svg [ viewBox "0 0 100 100", width "300px" ]
-      [ circle [ cx "50", cy "50", r "45", fill "#0B79CE" ] []
-      , line [ x1 "50", y1 "50", x2 handX, y2 handY, stroke "#023963" ] []
+    div [] [ svg [ viewBox "0 0 100 100", width "300px" ]
+              [ circle [ cx "50", cy "50", r "45", fill "#0B79CE" ] []
+              , line [ x1 "50", y1 "50", x2 handX, y2 handY, stroke "#023963" ] []
+              ]
+      , Html.text (toString <| Time.inMinutes model)
       ]
